7ce5d2f7bc47d5dda6563442a9abbe4e9aa7355a
muhammedmuneerk <muhammedmuneerk15@gmail.com>
2025-07-26 17:02:52 +0530

    make the site's scrolling more controlled, slow and smooth

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from './App'
 import './index.css'
 import theme from './theme'
 import 'lenis/dist/lenis.css'
+import Lenis from 'lenis'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -16,4 +17,19 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
-) 
+)
+
+// Initialize Lenis for smooth, controlled scrolling across the entire app
+const lenis = new Lenis({
+  // Higher duration slows down the scroll speed for a more controlled feel
+  duration: 1.5,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smooth: true,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf); 
